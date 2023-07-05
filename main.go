5218dc6d7b517c2595b1bb70834fea292a572602
NBDatsuya <2646809050@qq.com>
2023-07-06 02:21:25 +0800

    feat: Basic Server

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ type About struct {
 	version     string
 	description string
 	updateTime  string
+	since string
 }*/
 
 type Server struct {
@@ -24,7 +25,8 @@ func main() {
 			name:        "SeimonCore",
 			version:     "V0.0.1 Genesis Prototype",
 			description: "The Genesis of SeimonCore",
-			updateTime:  "2023-7-2T2:41:00",
+			updateTime:  "2023-7-6T2:22:00",
+			since: "2023-7-2T2:41:00",
 		}*/
 	server := NewServer("127.0.0.1", 2121)
 	server.Start()
